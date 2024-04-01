442d5a86376468630456dc0585197b87519e2b78
cvcvrril <imartinezrodriguez@educa.madrid.org>
2024-04-01 11:39:02 +0200

    Primeras configuraciones del servidor REST

---
+++ ServidorRestInesMR/src/main/java/com/example/servidorrestinesmr/ServidorRestInesMrApplication.java
@@ -0,0 +1,13 @@
+package com.example.servidorrestinesmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorRestInesMrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorRestInesMrApplication.class, args);
+	}
+
+}
---
+++ ServidorRestInesMR/src/test/java/com/example/servidorrestinesmr/ServidorRestInesMrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servidorrestinesmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorRestInesMrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
