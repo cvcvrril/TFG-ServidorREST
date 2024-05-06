64f86001c690108a2f7e6dcd65085d69d4d3918b
cvcvrril <imartinezrodriguez@educa.madrid.org>
2024-05-06 23:18:33 +0200

    Clase de testeos arreglada

--- ServidorRestInesMR/src/test/java/com/example/servidorrestinesmr/domain/services/impl/ServiceUbiImplTest.java
+++ ServidorRestInesMR/src/test/java/com/example/servidorrestinesmr/domain/services/impl/ServiceUbiImplTest.java
@@ -2,12 +2,17 @@ package com.example.servidorrestinesmr.domain.services.impl;
 
 import com.example.servidorrestinesmr.data.dao.DaoUbi;
 import com.example.servidorrestinesmr.data.model.entities.UbiEntity;
+import com.example.servidorrestinesmr.domain.model.NewUbiDTO;
 import com.example.servidorrestinesmr.domain.model.UbiDTO;
+import com.example.servidorrestinesmr.domain.model.error.ErrorSec;
 import com.example.servidorrestinesmr.domain.services.ServiceUbi;
 import io.vavr.control.Either;
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,10 +30,14 @@ import static org.mockito.Mockito.when;
 @RequiredArgsConstructor
 class ServiceUbiImplTest {
 
-    @Autowired
-    private ServiceUbi serviceUbi;
-    @MockBean
+    @Mock
     private DaoUbi daoUbi;
+    private ServiceUbi serviceUbi;
+
+    @BeforeEach
+    void setUp() {
+        serviceUbi = new ServiceUbiImpl(daoUbi);
+    }
 
 
     @Test
@@ -44,8 +53,81 @@ class ServiceUbiImplTest {
 
         // Llamar al método que estás probando
         List<UbiDTO> userEntityList = serviceUbi.getAll().getOrElseThrow(() -> new RuntimeException("Me cago en mi desgracia"));
+        Either<ErrorSec, List<UbiDTO>> result = serviceUbi.getAll();
 
         // Verificar el resultado esperado
-        assertThat(userEntityList).size().isPositive();
+        assertThat(result.get()).hasSize(2);
+        assertThat(result.isRight()).isTrue();
     }
+
+    @Test
+    void getByIdUbi() {
+        // Datos simulados
+        int id = 1;
+        UbiDTO mockedUbi = new UbiDTO(1, 40.123, -3.456);
+
+        // Configurar el comportamiento del DAO
+        when(daoUbi.getByIdUbi(id)).thenReturn(Either.right(mockedUbi));
+
+        // Llamar al método que estás probando
+        Either<ErrorSec, UbiDTO> result = serviceUbi.getByIdUbi(id);
+
+        // Verificar el resultado esperado
+        assertThat(result.isRight()).isTrue();
+        assertThat(result.get()).isEqualTo(mockedUbi);
+    }
+
+    @Test
+    void getAllByUserId() {
+        // Datos simulados
+        int userId = 1;
+        List<UbiDTO> mockedUbiList = new ArrayList<>();
+        mockedUbiList.add(new UbiDTO(1, 40.123, -3.456));
+        mockedUbiList.add(new UbiDTO(2, 40.789, -3.987));
+
+        // Configurar el comportamiento del DAO
+        when(daoUbi.getAllByUserId(userId)).thenReturn(Either.right(mockedUbiList));
+
+        // Llamar al método que estás probando
+        Either<ErrorSec, List<UbiDTO>> result = serviceUbi.getAllByUserId(userId);
+
+        // Verificar el resultado esperado
+        assertThat(result.isRight()).isTrue();
+        assertThat(result.get()).isEqualTo(mockedUbiList);
+    }
+
+    @Test
+    void deleteUbi() {
+        // Datos simulados
+        int id = 1;
+        int deletedCount = 1;
+
+        // Configurar el comportamiento del DAO
+        when(daoUbi.deleteUbi(id)).thenReturn(Either.right(deletedCount));
+
+        // Llamar al método que estás probando
+        Either<ErrorSec, Integer> result = serviceUbi.deleteUbi(id);
+
+        // Verificar el resultado esperado
+        assertThat(result.isRight()).isTrue();
+        assertThat(result.get()).isEqualTo(deletedCount);
+    }
+
+    @Test
+    void addUbi() {
+        // Datos simulados
+        NewUbiDTO newUbi = new NewUbiDTO( 40.123, -3.456, 1);
+        UbiDTO addedUbi = new UbiDTO(45, 40.123, -3.456);
+
+        // Configurar el comportamiento del DAO
+        when(daoUbi.addUbi(newUbi)).thenReturn(Either.right(addedUbi));
+
+        // Llamar al método que estás probando
+        Either<ErrorSec, UbiDTO> result = serviceUbi.addUbi(newUbi);
+
+        // Verificar el resultado esperado
+        assertThat(result.isRight()).isTrue();
+        assertThat(result.get()).isEqualTo(addedUbi);
+    }
+
 }
